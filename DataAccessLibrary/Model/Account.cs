a535fad2ced73111ccfc4ef3d85cba4fb445c398
Bugnaru Tudor Eduard <eduardbugnaru@gmail.com>
2024-04-15 12:59:25 +0300

    first entity added

---
+++ DataAccessLibrary/Model/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Model
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public String Email { get; set; }
+        public String Username { get; set; }
+        public String Password { get; private set; }
+        public bool IsAdult { get; set; }
+
+
+        public Account(string email, string username, string password, bool isAdult)
+        {
+            Email = email;
+            Username = username;
+            Password = password;
+            IsAdult = isAdult;
+        }
+    }
+}
