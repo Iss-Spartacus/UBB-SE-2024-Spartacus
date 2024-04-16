b3bca7f8cf2e2fb37a841b15e1a4038830c941ba
MadalinCernat <madalin.cernat02@gmail.com>
2024-04-16 16:40:01 +0300

    moved data access logic from login window to separate service

---
+++ ISSpartacusWPFApp/Service/LoginService.cs
@@ -0,0 +1,44 @@
+﻿using DataAccessLibrary.Modules;
+using DataAccessLibrary.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISSpartacusWPFApp.Service
+{
+    public class LoginService
+    {
+        public AccountType AccountType { get; private set; }
+
+        public string ValidateLogin(string email, string password)
+        {
+            if (!Validator.ValidateEmail(email))
+                return "Invalid email";
+
+            if (!Validator.ValidatePassword(password))
+                return "Invalid password";
+
+            ConfigurationLoader.Configuration config = new ConfigurationLoader.Configuration();
+            config.LoadFromJson("ConfigurationFile.json");
+
+            AccountRepository accountRepository = new AccountRepository(config);
+            AccountService accountService = new AccountService(accountRepository);
+
+            var accounts = accountService.GetAllEntitiesService();
+
+            foreach (var account in accounts)
+            {
+                if (account.Email == email && account.Password == password)
+                {
+                    AccountType = AccountVerifier.VerifyAccountType(email);
+                    return "Success";
+                }
+            }
+
+            return "Invalid email and password combination";
+        }
+    }
+
+}
--- ISSpartacusWPFApp/Views/Authentication/Login.xaml.cs
+++ ISSpartacusWPFApp/Views/Authentication/Login.xaml.cs
@@ -31,63 +31,38 @@ namespace ISSpartacusWPFApp.Views.Authentication
             string email = txtEmail.Text;
             string password = txtPassword.Password;
 
-            if(Validator.ValidateEmail(email) == false)
+            LoginService loginService = new LoginService();
+
+            string result = loginService.ValidateLogin(email, password);
+
+            if (result == "Success")
             {
-                MessageBox.Show("Invalid email", "Authentication problem", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-            if(Validator.ValidatePassword(password) == false)
-            {
-                MessageBox.Show("Invalid password", "Authentication problem", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
-
-            ConfigurationLoader.Configuration config = new ConfigurationLoader.Configuration();
-            config.LoadFromJson("ConfigurationFile.json");
-            AccountRepository accountRepository = new AccountRepository(config);
-            AccountService accountService = new AccountService(accountRepository);
-
-            var accounts = accountService.GetAllEntitiesService();
-
-            bool accountWasFound = false;
-            DataAccessLibrary.Model.Account foundAccount;
-            foreach(var account in accounts)
-            {
-                if(account.Email == email && account.Password == password)
-                {
-                    accountWasFound = true;
-                    foundAccount = account;
-                }
-            }
-
-            if(accountWasFound == true)
-            {
-                AccountType accountType = AccountVerifier.VerifyAccountType(email);
-
-                if (accountType == AccountType.User)
-                {
-                    ISSpartacusWPFApp.Views.User userView = new User();
-                    userView.Show();
-                    return;
-                }
-                else if (accountType == AccountType.Manager)
-                {
-                    ISSpartacusWPFApp.Views.Manager managerView = new Manager();
-                    managerView.Show();
-                    return;
-                }
-                else if (accountType == AccountType.Employee)
-                {
-                    ISSpartacusWPFApp.Views.Employee employeeView = new Employee();
-                    employeeView.Show();
-                    return;
-                }
+                OpenUserInterface(loginService.AccountType);
             }
             else
             {
-                MessageBox.Show("Invalid email and password combination", "Authentication problem", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show(result, "Authentication problem", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
+
+        private void OpenUserInterface(AccountType accountType)
+        {
+            if (accountType == AccountType.User)
+            {
+                ISSpartacusWPFApp.Views.User userView = new User();
+                userView.Show();
+            }
+            else if (accountType == AccountType.Manager)
+            {
+                ISSpartacusWPFApp.Views.Manager managerView = new Manager();
+                managerView.Show();
+            }
+            else if (accountType == AccountType.Employee)
+            {
+                ISSpartacusWPFApp.Views.Employee employeeView = new Employee();
+                employeeView.Show();
+            }
+        }
+
     }
 }
